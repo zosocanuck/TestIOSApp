1e25631a96015dc7036a202c478d97e2730ffbf0
Ivan Wallis <iwallis@gmail.com>
2022-06-30 12:43:04 -0700

    Initial Commit

---
+++ TestIOSApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TestIOSApp
+//
+//  Created by Ivan Wallis on 6/30/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TestIOSApp/TestIOSAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestIOSAppApp.swift
+//  TestIOSApp
+//
+//  Created by Ivan Wallis on 6/30/22.
+//
+
+import SwiftUI
+
+@main
+struct TestIOSAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
